b5634b077ae79ea5d9398c760847762782789f9d
amer <amerosama.jr@gmail.com>
2023-01-19 16:35:21 +0200

    path parm and query param

--- src/main/java/com/javabrains/advancedJAXRS/Resources/Resource.java
+++ src/main/java/com/javabrains/advancedJAXRS/Resources/Resource.java
@@ -1,23 +1,23 @@
 package com.javabrains.advancedJAXRS.Resources;
 
-
 import javax.inject.Singleton;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
-@Path("test")
-@Singleton
-public class Resource  {
-private int count;
-	
+@Path("{pathParam}/test")
+public class Resource {
+	@PathParam("pathParam") private String pathParam;
+	@QueryParam("query") private String queryParam;
+
 	@GET
 	@Produces(MediaType.TEXT_PLAIN)
 	public String welcome() {
-		count = count+1; 
-		return "Welcome to alien resource "+ count;
+		
+		return "it works ! path used :" + pathParam + "  and Query used : "+ queryParam;
 	}
-
 
 }
